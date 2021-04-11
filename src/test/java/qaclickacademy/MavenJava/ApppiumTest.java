cb267efcc6040e8bfd1a868f66c328d5ce62a1c8
rahul <rahuljsrsinha@gmail.com>
2021-04-11 22:46:12 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/ApppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class ApppiumTest {
+	
+	@Test
+	public void postJIRA()
+	{
+	System.out.println("POST JIRA ON Appium");	
+	}
+	
+	@Test
+	public void Deleteall()
+	{
+		System.out.println("POST JIRA ON Delete all");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/MavenJava/selaryTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class selaryTest {
+
+	//TestNG xml file from maven
+	// How to execute all test cases from test folder- 
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Browser Auto");
+	}
+	
+	@Test
+	public void ElementUi()
+	{
+		System.out.println("Element UI");
+		
+	}
+}
